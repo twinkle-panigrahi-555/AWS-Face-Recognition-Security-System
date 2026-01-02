94da359550b08f0ba21905ec1443912556ecb42b
Twinkle panigrahi <twinklepanigrahi766@gmail.com>
2026-01-02 12:39:54 +0530

    Create employee_info.py

---
+++ employee_info.py
@@ -0,0 +1,15 @@
+employees = {
+    "Bhumi": {"Department": "HR", "Phone": "9876543210"},
+    "Deepak": {"Department": "Finance", "Phone": "9865321470"},
+    "Rohit": {"Department": "IT", "Phone": "9123456701"},
+    "Sneha": {"Department": "Admin", "Phone": "9988776655"},
+    "kabir": {"Department": "IT Support", "Phone": "9090909090"},
+    "laxmi": {"Department": "Sales", "Phone": "9000001111"},
+    "rohan": {"Department": "IT", "Phone": "9898989898"},
+    "roshini": {"Department": "Security", "Phone": "9988447733"},
+    "sania": {"Department": "Marketing", "Phone": "9776655443"},
+    "sonali": {"Department": "Reception", "Phone": "9993452176"},
+    "subham": {"Department": "DevOps", "Phone": "9456123789"},
+    "subhasree": {"Department": "Design", "Phone": "9555784123"},
+    "suman": {"Department": "Management", "Phone": "9666123450"}
+}
